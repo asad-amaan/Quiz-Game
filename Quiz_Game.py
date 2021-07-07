5b88faee8109d1ea75b39a6811f46c993cbd090c
asad-amaan <87056336+asad-amaan@users.noreply.github.com>
2021-07-07 10:53:36 +0530

    Add files via upload

--- Quiz_Game.py
+++ Quiz_Game.py
@@ -3,36 +3,36 @@ option = " "
 
 option = input("Do you want to play? (Yes/No)   ")
 
-if option != "Yes": 
+if option.lower() != "yes": 
     quit()
 
 print("Let's Play!")
 marks = 0 
 answer = input("Which was the first fully supported 64-bit operating system?  ")
-if answer == 'linux':
+if answer.lower() == 'linux':
     print("Correct!")
     marks +=1
 else:
     print("Incorrect, the correct answer is : linux")
 answer = input("What protocol is used to recieve mail? ")
-if answer == "POP3":
+if answer.lower() == "pop3":
     print("Correct!")
     marks+=1
 else:
     print("Incorrect, the correct answer is : POP3")
 
 answer = input("Which type of number system does the computer use to store data?  ")
-if answer == "binary":
+if answer.lower() == "binary":
     print("Correct!")
     marks+=1 
 else:
     print("Incorrect, the correct answer is : binary")
 
 answer = input("Which data type is immutable?  ")
-if answer == "string":
+if answer.lower() == "string":
     print("Correct!")
     marks+=1
-elif answer == "tuple":
+elif answer.lower() == "tuple":
     print("Correct!")
     marks+=1
 else:
